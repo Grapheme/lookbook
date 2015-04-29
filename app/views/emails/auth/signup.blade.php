59daf171cba6736caf4084cbfb81e2c7111a3863
Владимир <vkharseev@gmail.com>
2015-04-29 14:02:44 +0300

    Изменить письмо с подтверждением регистрации

--- app/views/emails/auth/signup.blade.php
+++ app/views/emails/auth/signup.blade.php
@@ -6,15 +6,10 @@
 <body>
 	<div>
 		<p>
-			Добро пожаловать в {{ link_to('','LookBook.pro') }}.<br>
-			Активируйте свой аккаунт, перейдя по <a href="{{ URL::route('signup-activation',$account->temporary_code) }}">ссылке</a>.<br>
-			Не откладывайте, ссылка действует 72 часа.
+            Вы только что зарегистрировались на {{ link_to('','look-book.ru') }}.<br>
+            Вам осталось лишь активировать свою учетную запись, пройдя по <a href="{{ URL::route('signup-activation',$account->temporary_code) }}">ссылке</a>.<br>
+            Не откладывайте, ссылка действует 72 часа.
 		</p>
-        <p>
-            Для авторизации воспользуйтесь логином и паролем:<br>
-            Логин: {{ $account->email }}<br>
-            Пароль: {{ Input::get('password') }}
-        </p>
 	</div>
 </body>
 </html>
