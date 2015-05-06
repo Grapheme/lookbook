af7c651345b053cfd726b5741850d4a0cbad1c14
Владимир <vkharseev@gmail.com>
2015-05-06 15:06:16 +0300

    Все пользователи. Страница поста. Список постов по теме

--- app/views/templates/guest/blocks/top_user_posts.blade.php
+++ app/views/templates/guest/blocks/top_user_posts.blade.php
@@ -27,9 +27,11 @@ endforeach;
     <div class="right-content">
         <ul class="right-content__list list-big">
             @foreach($top_posts as $top_post)
+                @if(count($top_post->views))
                 <li class="list__item">
                     @include(Helper::layout('assets.top_post'),array('top_post'=>$top_post,'categories'=>$categories))
                 </li>
+                @endif
             @endforeach
         </ul>
     </div>
