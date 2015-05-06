30529162889dc5bdca8070f93a0ed1b561aef611
Владимир <vkharseev@gmail.com>
2015-05-06 15:05:31 +0300

    Все пользователи. Страница поста. Список постов по теме

--- app/views/templates/guest/blocks/top_posts.blade.php
+++ app/views/templates/guest/blocks/top_posts.blade.php
@@ -8,9 +8,11 @@ $top_posts = PostViews::select(DB::raw('post_id, count(*) as user_count'))->grou
     <div class="right-content">
         <ul class="right-content__list list-big">
             @foreach($top_posts as $top_post)
+                @if(count($top_post->views))
                 <li class="list__item">
                     @include(Helper::layout('assets.top_post'),array('top_post'=>$top_post->post,'categories'=>$categories))
                 </li>
+                @endif
             @endforeach
         </ul>
     </div>
