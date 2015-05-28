f85b321dda5eade09bf17fb074212fd5bbb82eb1
Владимир <vkharseev@gmail.com>
2015-05-28 17:28:12 +0300

    МОЙ БЛОГ-ЛИСТ

--- app/views/templates/guest/assets/avatar.blade.php
+++ app/views/templates/guest/assets/avatar.blade.php
@@ -6,11 +6,14 @@ $hasAvatar = FALSE;
 if(!empty($user['photo']) && File::exists(public_path($user['photo']))):
     $hasAvatar = TRUE;
 endif;
+if(!isset($showName)):
+    $showName = TRUE;
+endif;
 ?>
 <a href="{{ URL::route('user.profile.show',$user['id'].'-'.BaseController::stringTranslite($user['name'])) }}">
-    @if(isset($no_avatar) && $no_avatar === TRUE)
-        {{ $user['name'] }}
-    @else
+@if(isset($no_avatar) && $no_avatar === TRUE)
+    {{ $user['name'] }}
+@else
     <span class="author__photo">
         <span data-empty-name="{{ $user['name'] }}" class="profile-ava ava-min{{ !$hasAvatar ? ' ava-empty ' : '' }}">
             @if($hasAvatar)
@@ -19,6 +22,8 @@ endif;
             <div class="ava-image__empty"><span class="js-empty-chars"></span></div>
         </span>
     </span>
+    @if($showName)
     <span class="profile-name"><span>{{ $user['name'] }}</span></span>
     @endif
+@endif
 </a>
--- app/views/templates/guest/blocks/subscribes/blogs.blade.php
+++ app/views/templates/guest/blocks/subscribes/blogs.blade.php
@@ -15,7 +15,7 @@ endif;
 ?>
 @foreach($blogs as $blog)
     <li class="list__item js-post">
-    @include(Helper::layout('assets.avatar'),array('user'=>$blog))
+    @include(Helper::layout('assets.avatar'),array('user'=>$blog,'showName'=>FALSE))
     @if($blog->brand == 0)
         @if(count($top_blogger_ids) && in_array($blog->id,$top_blogger_ids))
             <div class="item__best-blogger"></div>
@@ -26,6 +26,9 @@ endif;
         @endif
     @endif
         <div class="item__content">
+            <div class="content__title">
+                <a href="{{ URL::route('user.profile.show',$blog->id.'-'.BaseController::stringTranslite($blog->name)) }}">{{ $blog->name }}</a>
+            </div>
             <div class="content__followers">
                 <b>{{ $blog->me_signed->count() }}</b> {{ Lang::choice('подписчик|подписчика|подписчиков',$blog->me_signed->count()) }}
             </div>
