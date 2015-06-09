9411e4c5195ccae8a419405f8b1826d0fcb36c78
Владимир <vkharseev@gmail.com>
2015-06-09 16:58:24 +0300

    Загрузка аватарки

--- app/modules/uploads/admin_uploads.controller.php
+++ app/modules/uploads/admin_uploads.controller.php
@@ -413,9 +413,9 @@ class AdminUploadsController extends BaseController {
 
         $base62string = Input::get($input);
         if(!empty($base62string)):
-            list($type, $base62string) = explode(';', $base62string);
-            list(, $base62string)      = explode(',', $base62string);
-            $base62string = base64_decode($base62string);
+            #list($type, $base62string) = explode(';', $base62string);
+            #list(, $base62string)      = explode(',', $base62string);
+            #$base62string = base64_decode($base62string);
             $fileName = time()."_".Auth::user()->id."_".rand(1000, 1999).'.png';
             ImageManipulation::make($base62string)->save(public_path(Config::get('site.uploads_thumb_user_dir')).'/thumb_'.$fileName);
             ImageManipulation::make($base62string)->save(public_path(Config::get('site.uploads_image_user_dir')).'/'.$fileName);
