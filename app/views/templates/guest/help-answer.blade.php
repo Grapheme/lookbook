55b9fd5be06d962d6e49c2e38612fc61ade2c938
Владимир <vkharseev@gmail.com>
2015-06-17 14:01:37 +0300

    Страница вопроса

--- app/views/templates/guest/help-answer.blade.php
+++ app/views/templates/guest/help-answer.blade.php
@@ -14,7 +14,7 @@
         <div class="container_12">
             <div class="grid_12">
                 <div class="category-header header-lite">
-                    <div class="header__title">Помощь</div>
+                    <div class="header__title">{{ $page->seo->h1 }}</div>
                 </div>
             </div>
             <div class="clearfix"></div>
@@ -22,35 +22,31 @@
                 <div class="dashboard-tab">
                     <div class="reg-content__left">
                         <div class="one-question">
-                            <div class="question__title">Чем отличается аккаунт компании от персональной учетной записи?
+                            <div class="question__title">
+                                {{ isset($page->blocks['question']['name']) ? $page->blocks['question']['name'] : '' }}
                             </div>
                             <div class="question-cont">
                                 <div class="question__answer">
-                                    В 1947 году, когда Европа только начала восстанавливаться после пятилетней войны,
-                                    Кристиан Диор создал силуэт, который навсегда вошел в историю под названием New Look.
-                                    Этим, без сомнения, главным своим изобретением дизайнер сделал выдающийся вклад в
-                                    мировую моду. Throughout my sophomore to senior year in college, I volunteered as a
-                                    counselor at the Berkeley Free Clinic providing mental health services to the homeless
-                                    community and also as a visiting teacher at Harding Elementary working with
-                                    underprivileged students. After college, I continued to work with marginalized
-                                    communities, mainly as an educator. I volunteered at the San Francisco Juvenile
-                                    Detention Center, working with incarcerated youths in the maximum security unit, for
-                                    over a year and a half.
-                                    Most of these youths, like me, were from broken homes and/or foster care. I taught the
-                                    high school curriculum with the goal of helping these youths pass the high school exit
-                                    exam, so they’ll have a second chance in life once they leave juvenile hall. Currently,
-                                    I’m based in Los Angeles and volunteer at InsideOutWriters, a non-profit organization
-                                    that provides mentorship to previously incarcerated youths.
+                                    {{ isset($page->blocks['question']['content']) ? $page->blocks['question']['content'] : '' }}
                                 </div>
-                                <div class="question__btn"><a href="help.html" class="white-btn action-question"><i
-                                                class="svg-icon icon-question"></i>Вернуться к списку вопросов</a></div>
+                                <div class="question__btn">
+                                    <a href="{{ pageurl('help') }}" class="white-btn action-question">
+                                        <i class="svg-icon icon-question"></i>Вернуться к списку вопросов
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div class="reg-content__right right-links">
-                        <div class="right-title"><a href="about.html" class="active">О проекте</a></div>
-                        <div class="right-title"><a href="help.html">Помощь</a></div>
-                        <div class="right-title"><a href="contact-us.html">Связаться с нами</a></div>
+                        <div class="right-title">
+                            <a href="{{ pageurl('about') }}">О проекте</a>
+                        </div>
+                        <div class="right-title">
+                            <a href="{{ pageurl('help') }}" class="active">Помощь</a>
+                        </div>
+                        <div class="right-title">
+                            <a href="{{ pageurl('contacts') }}">Связаться с нами</a>
+                        </div>
                     </div>
                     <div class="clearfix"></div>
                 </div>
