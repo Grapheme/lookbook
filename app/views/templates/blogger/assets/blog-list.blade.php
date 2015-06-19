de85785a438f83a906ddc9c0e0ffb27eceb882c4
Владимир <vkharseev@gmail.com>
2015-06-19 17:49:25 +0300

    -top post

--- app/views/templates/blogger/assets/blog-list.blade.php
+++ app/views/templates/blogger/assets/blog-list.blade.php
@@ -10,6 +10,6 @@
         </ul>
         <a href="{{ URL::route('blogger-blog-list') }}" class="right-content__all-link">All blogs</a>
     @else
-        Вы еще не добавили ни один блог в свой блог-лист.<br>Читайте <a href="javascript:void(0);">TOP POST</a> и выбирайте любимые.
+        Вы еще не добавили ни один блог в свой блог лист.
     @endif
 </div>
