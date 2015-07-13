c2c6d70d21c5af7da5ee3c3978d94925aef6ee60
sngrl <reserved@mail.ru>
2015-07-13 17:33:44 +0300

    fix

--- app/config/az/database.php
+++ app/config/az/database.php
@@ -13,7 +13,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'grapheme-cms',
+			'database'  => 'lookbook',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
