a3128fdb948fd2f38ee6dc3498e22a58a8145b83
Владимир <vkharseev@gmail.com>
2015-07-21 11:34:39 +0300

    Имейлы для обратной связи

--- app/config/mail.php
+++ app/config/mail.php
@@ -3,15 +3,16 @@
 return array(
 
     'feedback' => array(
-        'address' => ' support@look-book.ru',
+        'address' => 'support@look-book.ru',
     ),
 
     'driver' => 'smtp',
     'host' => 'in.mailjet.com',
     'port' => 587,
     'from' => array(
-        'address' => 'support@grapheme.ru',
-        'name' => 'LookBook'
+        'address' => 'support@look-book.ru',
+        #'address' => 'support@grapheme.ru',
+        'name' => 'LOOKBOOK'
     ),
     'username' => '0d8dd8623bd38b41c43683c41c0558eb',
     'password' => '465c500abd5f680f0b20405deb967b36',
