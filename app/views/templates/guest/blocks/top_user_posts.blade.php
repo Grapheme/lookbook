3f55aecd4541828ce49f201c1a198f99a03b9a69
Владимир <vkharseev@gmail.com>
2015-07-29 14:38:06 +0300

    Упорядочить TOP POST по количеству просмотров

--- app/views/templates/guest/blocks/top_user_posts.blade.php
+++ app/views/templates/guest/blocks/top_user_posts.blade.php
@@ -1,7 +1,7 @@
 <?php
 $top_posts = $top_posts_ids_all = $top_posts_ids = array();
 foreach (Post::where('user_id', $user_id)->where('publication', 1)->with('views')->get() as $post):
-    $top_posts_ids_all[$post->id] = count($post->views);
+    $top_posts_ids_all[$post->id] = count($post->views) + $post->guest_views;
 endforeach;
 if (count($top_posts_ids_all)):
     arsort($top_posts_ids_all);
@@ -15,7 +15,13 @@ if (count($top_posts_ids_all)):
     foreach(Post::whereIn('id', array_keys($top_posts_ids))->where('user_id', $user_id)->where('publication', 1)->with('user', 'views', 'photo')->get() as $post):
         $top_posts[$post->id] = $post;
     endforeach;
-    array_multisort($top_posts,array_keys($top_posts_ids));
+    $tmp_posts = $top_posts;
+    $top_posts = array();
+    foreach($top_posts_ids as $index => $top_post):
+        if(isset($tmp_posts[$index])):
+            $top_posts[$index] = $tmp_posts[$index];
+        endif;
+    endforeach;
 endif;
 $categories = array();
 foreach(Dic::where('slug','categories')->first()->values as $category):
