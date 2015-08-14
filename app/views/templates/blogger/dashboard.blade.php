7824addbd8aa64ac1262d52134b4f23ab486026c
Владимир <vkharseev@gmail.com>
2015-08-14 17:23:33 +0300

    Рекламный пост- на 2 и 6 место Рекламный банер на 4 и 8 место

--- app/views/templates/blogger/dashboard.blade.php
+++ app/views/templates/blogger/dashboard.blade.php
@@ -14,8 +14,24 @@ if ($blogsIDs = BloggerSubscribe::where('user_id', Auth::user()->id)->orderBy('u
     $blog_list = Accounts::where('group_id', 4)->where('active', 1)->whereIn('id', $blogsIDs)->take(5)->get();
     $posts_total_count = Post::whereIn('user_id', $blogsIDs)->where('in_advertising', 0)->count();
     $posts = Post::whereIn('user_id', $blogsIDs)->where('in_advertising', 0)->where('publication', 1)->orderBy('publish_at', 'DESC')->orderBy('id', 'DESC')->with('user', 'photo', 'tags_ids', 'views', 'likes', 'comments')->take($post_limit)->get();
-    $posts_advertising = Post::whereIn('user_id', $blogsIDs)->where('in_advertising', 1)->where('publication', 1)->orderBy('publish_at', 'DESC')->orderBy('id', 'DESC')->with('user', 'photo', 'tags_ids', 'views', 'likes', 'comments')->take(1)->get();
+    $posts_advertising = Post::where('in_advertising', 1)->where('publication', 1)->orderBy('publish_at', 'DESC')->orderBy('id', 'DESC')->with('user', 'photo', 'tags_ids', 'views', 'likes', 'comments')->take(2)->get();
     $promo_posts = PostPromo::where('position', 0)->where('in_line', 1)->orderBy('order')->with('photo')->get();
+    $current_posts_count = count($posts);
+    $posts_view = array();
+    foreach($posts as $index => $post):
+        if($index == 1 && isset($posts_advertising[0])):
+            $posts_view[] = View::make(Helper::layout('blocks.posts-advertising'), array('posts' => array($posts_advertising[0])))->render();
+        elseif($index == 3 && isset($promo_posts[0])):
+            $posts_view[] = View::make(Helper::layout('blocks.posts-promo'), array('posts' => array($promo_posts[0])))->render();
+        elseif($index == 5 && isset($posts_advertising[1])):
+            $posts_view[] = View::make(Helper::layout('blocks.posts-advertising'), array('posts' => array($posts_advertising[1])))->render();
+        elseif($index == 7 && isset($promo_posts[1])):
+            $posts_view[] = View::make(Helper::layout('blocks.posts-promo'), array('posts' => array($promo_posts[1])))->render();
+        else:
+            $posts_view[] = View::make(Helper::layout('blocks.posts'), array('posts' => array($post)))->render();
+        endif;
+    endforeach;
+    $posts = $posts_view;
 endif;
 ?>
 @extends(Helper::acclayout())
@@ -35,11 +51,14 @@ endif;
                     <div class="reg-content__left">
                         @if(count($posts))
                             <ul class="dashboard-list js-posts">
-                                @include(Helper::layout('blocks.posts'),compact('posts','categories'))
-                                @include(Helper::layout('blocks.posts-advertising'), array('posts'=>$posts_advertising,'categories'=>$categories))
-                                @include(Helper::layout('blocks.posts-promo'),array('posts'=>$promo_posts))
+                            @foreach($posts as $post)
+                                {{ $post }}
+                            @endforeach
+                                {{--@include(Helper::layout('blocks.posts'),compact('posts','categories'))--}}
+                                {{--@include(Helper::layout('blocks.posts-advertising'), array('posts'=>$posts_advertising,'categories'=>$categories))--}}
+{{--                                @include(Helper::layout('blocks.posts-promo'),array('posts'=>$promo_posts))--}}
                             </ul>
-                            @if($posts_total_count > count($posts))
+                            @if($posts_total_count > $current_posts_count)
                                 @include(Helper::layout('assets.more_post'),array('user'=>Auth::user()->id,'post_limit'=>$post_limit,'route_name'=>'post.public.more.subscribes'))
                             @endif
                         @else
