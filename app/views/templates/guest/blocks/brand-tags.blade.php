c0c57d3e56c8175b2bc0a8e0882d255d05155c87
Владимир <vkharseev@gmail.com>
2015-08-27 15:56:24 +0300

    увеличить лимит на размер изображение-обложки

--- app/views/templates/guest/blocks/brand-tags.blade.php
+++ app/views/templates/guest/blocks/brand-tags.blade.php
@@ -1,26 +1,34 @@
 @if($brand_tags = BrandTags::where('user_id', Auth::user()->id)->with('photo', 'posts')->orderBy('title')->get())
+    <?php $showTags = FALSE;?>
+    @foreach($brand_tags as $tag)
+        @if(count($tag->posts))
+            <?php $showTags = TRUE;?>
+        @endif
+    @endforeach
     <?php
     $nickname = $user->id . '-' . BaseController::stringTranslite($user->name);
     if (!empty($user->nickname)):
         $nickname = $user->nickname;
     endif;
     ?>
-    <div class="right-title">Теги</div>
-    <div class="right-content">
-        <ul class="right-tags">
-            @foreach($brand_tags as $tag)
-                @if(count($tag->posts))
-                <?php $imageBackground = ''; ?>
-                <?php  $tag_link = $tag->id . '-' . BaseController::stringTranslite($tag->title); ?>
-                @if(!empty($tag->photo) && File::exists(Config::get('site.galleries_photo_dir') . '/' . $tag->photo->name))
-                    <?php $imageBackground = 'style="background-image: url(' . asset(Config::get('site.galleries_photo_public_dir') . '/' . $tag->photo->name) . ')"'; ?>
-                @endif
-                <a href="{{ URL::route('user.posts.tag.show', array($nickname, $tag_link)) }}"
-                   {{ $imageBackground }} class="tags__item">
-                    <span class="item__text"><span>{{ $tag->title }}</span></span>
-                </a>
-                @endif
-            @endforeach
-        </ul>
-    </div>
+    @if($showTags)
+        <div class="right-title">Теги</div>
+        <div class="right-content">
+            <ul class="right-tags">
+                @foreach($brand_tags as $tag)
+                    @if(count($tag->posts))
+                        <?php $imageBackground = ''; ?>
+                        <?php  $tag_link = $tag->id . '-' . BaseController::stringTranslite($tag->title); ?>
+                        @if(!empty($tag->photo) && File::exists(Config::get('site.galleries_photo_dir') . '/' . $tag->photo->name))
+                            <?php $imageBackground = 'style="background-image: url(' . asset(Config::get('site.galleries_photo_public_dir') . '/' . $tag->photo->name) . ')"'; ?>
+                        @endif
+                        <a href="{{ URL::route('user.posts.tag.show', array($nickname, $tag_link)) }}"
+                           {{ $imageBackground }} class="tags__item">
+                            <span class="item__text"><span>{{ $tag->title }}</span></span>
+                        </a>
+                    @endif
+                @endforeach
+            </ul>
+        </div>
+    @endif
 @endif
