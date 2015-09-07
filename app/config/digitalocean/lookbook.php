50c81ae2cd3c05f20cc752ddb663fb064c530189
Владимир <vkharseev@gmail.com>
2015-09-07 14:59:11 +0300

    index +

--- app/config/digitalocean/lookbook.php
+++ app/config/digitalocean/lookbook.php
@@ -2,8 +2,10 @@
 
 return array(
     'posts_limit' => 10,
-    'blogs_limit' => 3,
+    'blogs_limit' => 10,
     'accounts_limit' => 20,
     'count_top_posts' => 5,
-    'count_top_bloggers' => 5
+    'count_top_bloggers' => 5,
+
+    'main_enabled' => TRUE
 );
