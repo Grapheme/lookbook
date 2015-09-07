b19d5cd0c7620680b578768f45ab48a188e94304
Владимир <vkharseev@gmail.com>
2015-09-07 16:28:42 +0300

    В рекомендованные блоги (справа в личном кабинете) выводить 5 объектов

--- app/views/templates/blogger/assets/recommended-list.blade.php
+++ app/views/templates/blogger/assets/recommended-list.blade.php
@@ -1,5 +1,5 @@
 <?
-$recommended_blogs = Accounts::where('group_id', 4)->where('recommended', 1)->where('active', 1)->take(3)->get();
+$recommended_blogs = Accounts::where('group_id', 4)->where('recommended', 1)->where('active', 1)->take(5)->get();
 ?>
 @if(count($recommended_blogs))
 <div class="right-title">Рекомендованные блоги</div>
